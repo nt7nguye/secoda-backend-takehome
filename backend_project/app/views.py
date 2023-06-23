f0272fdd6f85f815f79e2fd247f22927e0fb9039
Tan Nguyen <45516852+nt7nguye@users.noreply.github.com>
2023-06-23 15:46:13 +0700

    reflecting all tables from different schema

--- backend_project/app/views.py
+++ backend_project/app/views.py
@@ -9,45 +9,59 @@ def connectView(request, db_url):
     except Exception as err:
         return HttpResponse(f'Error: {err}')
 
-    # Get a metadata object for the database 
-    metadata = MetaData()
-    metadata.reflect(bind=engine)
-
-
     # Crafting the response
     dbMetadata = []
     dbName = engine.url.database
-    
-    # Iterating through the metadata of each table in db
-    for table in metadata.tables.values():
-        # Parse columns
-        colMetadata = []
-        for col in table.columns.values():
-            colMetadata.append({
-                'name': col.name,
-                'type': str(col.type)
-            })
 
-        # Parse number of rows
-        num_rows = table.info.get('rows')
-        if num_rows is None:
-            # If the number of rows is not specified, query the table
-            with engine.connect() as conn:
-                try:
-                    query = select(func.count()).select_from(text(table.name))
-                    num_rows = conn.execute(query).scalar()
-                except Exception as err:
-                    # Display -1 if the query fails 
-                    num_rows = -1 
-                    print(err)
+    # Get the schema names using a SQL query
+    schemaNames = []
+    with engine.connect() as conn:
+        query = "SELECT schema_name FROM information_schema.schemata"
+        result = conn.execute(text(query))
 
-        # Craft the table metadata     
-        tableMetadata = {
-            'columns': colMetadata, 
-            'num_rows': num_rows, 
-            'schema': table.schema,
-            'database': dbName
-        }
+        # Iterate over the result and appends schema names to the list
+        for row in result:
+            schemaNames.append(row[0])
 
-        dbMetadata.append(tableMetadata)
+        # Close the resultset
+        result.close()
+
+        # Get a metadata object for the database 
+        metadata = MetaData()
+
+        # Get all tables in each schema 
+        for schemaName in schemaNames:
+            # Reflect the database schema
+            metadata.reflect(engine, schema=schemaName)
+
+            # Iterating through the metadata of each table in db
+            for table in metadata.tables.values():
+                # Parse columns
+                colMetadata = []
+                for col in table.columns.values():
+                    colMetadata.append({
+                        'name': col.name,
+                        'type': str(col.type)
+                    })
+
+                # Parse number of rows
+                num_rows = table.info.get('rows')
+                if num_rows is None:
+                    # If the number of rows is not specified, query the table
+                    try:
+                        query = select(func.count()).select_from(text(table.name))
+                        num_rows = conn.execute(query).scalar()
+                    except Exception as err:
+                        # Display -1 if the query fails 
+                        num_rows = -1 
+
+                # Craft the table metadata     
+                tableMetadata = {
+                    'columns': colMetadata, 
+                    'num_rows': num_rows, 
+                    'schema': schemaName,
+                    'database': dbName
+                }
+
+                dbMetadata.append(tableMetadata)
     return JsonResponse({'metadata': dbMetadata})
